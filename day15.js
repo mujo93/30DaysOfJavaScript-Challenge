4d069ee221854cbf515965b004b34565345d9e23
Mucahit Bayrak <34121683+mujo93@users.noreply.github.com>
2022-10-11 14:19:45 +0300

    test commit

---
+++ day15.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(firstName, lastName) {
+    console.log(this) // Check the output from here
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
+const person = new Person()
+
+console.log(person)
